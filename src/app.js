fd5561d0f9d6fefdb69fda9f324836e6706e9a30
Ilias Dinia <iliasd752@gmail.com>
2022-10-12 17:36:40 +0000

    Domain name generator is working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "small", "crazy"];
+  let noun = ["king", "lizard", "philosopher", "faker"];
+  let domain = [".com", ".net", ".us", ".io", ".fr", ".es", ".ch", ".co.uk"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.querySelector("#domain-results").innerHTML = domainName;
 };
