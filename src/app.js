d80208bdf97d8d7e91c60ced14fdf770b5f542dd
Ilias Dinia <iliasd752@gmail.com>
2022-10-12 18:12:15 +0000

    Outputting the domain names in a list

--- src/app.js
+++ src/app.js
@@ -6,21 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let domainName = [];
+  let domainNames = [];
   let pronoun = ["the", "our"];
   let adj = ["great", "big", "small", "crazy"];
   let noun = ["king", "lizard", "philosopher", "faker"];
-  let domain = [".com", ".net", ".us", ".io", ".fr", ".es", ".ch", ".co.uk"];
+  let domain = [".com", ".net", ".us", ".fr", ".es", ".ch", ".co.uk"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < domain.length; l++) {
-          domainName.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+          domainNames.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
         }
       }
     }
   }
-  console.log(domainName);
-  document.querySelector("#domain-results").innerHTML = domainName;
+  console.log(domainNames);
+
+  let outputDomains = "";
+
+  for (let domain of domainNames) {
+    outputDomains += `<li>${domain}</li>`;
+  }
+  document.querySelector("#domain-results").innerHTML = outputDomains;
 };
